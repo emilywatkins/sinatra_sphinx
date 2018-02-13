a3e70d87cae9cd0a404cd30bea613ef37380e76a
Andrew Peterson and Emily Watkins <emily@>
2018-02-13 10:27:39 -0800

    finish

--- app.rb
+++ app.rb
@@ -7,3 +7,16 @@ require('./lib/sphinx')
 get ('/') do
   erb(:input)
 end
+
+
+post ('/output') do
+  @answer1 = params.fetch("riddle1").downcase
+  @answer2 = params.fetch("riddle2").downcase
+  sphinx = Sphinx.new(@answer1, @answer2)
+  if sphinx.riddle? == true
+    @resultstring = "YOU'RE A LOSER (but you got it right)"
+  elsif sphinx.riddle? == false
+    @resultstring = "YOU'RE A LOSER"
+  end
+  erb(:output)
+end
--- spec/sphinx_spec.rb
+++ spec/sphinx_spec.rb
@@ -1,6 +1,7 @@
 require('rspec')
 require('sphinx')
 
+
 describe(Sphinx) do
   describe("#riddle") do
     it("Returns true if answer is correct") do
