8d16d64694ddbf145c08fe5e95bcf1fc1a4da50b
Andrew Peterson and Emily Watkins <andrew@>
2018-02-13 10:08:47 -0800

    add success condition

--- lib/sphinx.rb
+++ lib/sphinx.rb
@@ -1,10 +1,17 @@
 class Sphinx
-  def initialize(answer)
-    @answer = answer
+  def initialize(answer1, answer2)
+    @answer1 = answer1
+    @answer2 = answer2
   end
 
-  def riddle
-
+  def riddle?
+    answer1 = "snail"
+    answer2 = "short"
+    if @answer1 == answer1 && @answer2 == answer2
+      true
+    elsif @answer1 != answer1 || @answer2 != answer2
+      false
+    end
   end
 
 end
--- spec/sphinx_spec.rb
+++ spec/sphinx_spec.rb
@@ -3,14 +3,11 @@ require('sphinx')
 
 describe(Sphinx) do
   describe("#riddle") do
-    it("Returns true if answer is 'snail'") do
-      sphinx = Sphinx.new("snail")
-      expect(sphinx.riddle()).to(eq(true))
+    it("Returns true if answer is correct") do
+      sphinx = Sphinx.new("snail", "short")
+      expect(sphinx.riddle?()).to(eq(true))
     end
-    it("Returns true if answer is 'short'") do
-      sphinx = Sphinx.new("short")
-      expect(sphinx.riddle()).to(eq(true))
-    end
+
 
   end
 end
